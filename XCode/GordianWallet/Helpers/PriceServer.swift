bf52ce823537896cfaf415d073588439eae571f9
Fonta1n3 <fontainedenton@googlemail.com>
2020-10-07 08:51:09 +0800

    fix: fix PriverServer merge conflict and a bug which resulted in never ending spinner when creating psbt

--- XCode/GordianWallet/Helpers/PriceServer.swift
+++ XCode/GordianWallet/Helpers/PriceServer.swift
@@ -10,23 +10,37 @@ import Foundation
 import UIKit
 
 class PriceServer {
-
+    
     let localeConfig = LocaleConfig()
-
+    
     let defaultServerString: String = "h6zwwkcivy2hjys6xpinlnz2f74dsmvltzsd4xb42vinhlcaoe7fdeqd.onion"
     let defaultServerIndex: Int = 0
     let defaultServers: [String] = ["h6zwwkcivy2hjys6xpinlnz2f74dsmvltzsd4xb42vinhlcaoe7fdeqd.onion"]
-    let exchangeList: [String] = ["binance", "bitfinex", "coinbase", "kraken"]
-    let defaultExchange: String = "coinbase"
-
+    let exchangeList: [String] = ["Binance", "Bitbank", "Bitfinex", "Bithumb", "Bitstamp", "Bittrex", "CEX", "Coinbase", "Coinbase Pro", "Coincheck", "Coinone", "FTX", "Gemini", "Indodax", "Kraken", "Liquid", "OKCoin", "Upbit", "Zaif", "bitFlyer"]
+    let defaultExchange: String = "Coinbase"
+    let localeToExchange: [String:[String]] = [
+        "USD": ["Average", "Bitfinex", "bitFlyer", "Bitstamp", "Bittrex", "Coinbase", "Coinbase Pro", "FTX", "Gemini", "Kraken", "Liquid", "OKCoin"],
+        "GBP": ["Average", "Binance", "Bitfinex", "Bitstamp", "Bitstamp", "CEX", "Coinbase", "Coinbase Pro", "Kraken"],
+        "EUR": ["Average", "Bitfinex", "bitFlyer", "Bitstamp", "Bittrex", "Coinbase", "Coinbase Pro", "Kraken", "Liquid"],
+        "JPY": ["Average", "Bitbank", "bitFlyer", "Coinbase", "Coincheck", "Liquid", "Zaif"],
+        "AUD": ["Binance", "Coinbase", "Kraken", "Liquid"],
+        "BRL": ["Coinbase", "FTX"],
+        "KRW": ["Bithumb", "Coinbase", "Coinone", "Upbit"],
+        "ZAR": ["Binance", "Coinbase"],
+        "TRY": ["Binance", "Coinbase"],
+        "INR": ["Coinbase"],
+        "CAD": ["Coinbase", "Kraken"],
+        "IDR": ["Coinbase", "Indodax"]
+    ]
+    
     func getServers() -> [String] {
         return UserDefaults.standard.stringArray(forKey: "priceServers") ?? defaultServers
     }
-
+    
     func getDefaultExchange() -> String {
         return localeToExchange[localeConfig.getSavedLocale()]?[0] ?? defaultExchange
     }
-
+    
     func onStartup() -> Void {
         if UserDefaults.standard.stringArray(forKey: "priceServers") == nil {
             UserDefaults.standard.set(defaultServers, forKey: "priceServers")
@@ -39,30 +53,30 @@ class PriceServer {
             UserDefaults.standard.set(self.getDefaultExchange(), forKey: "currentExchange")
         }
     }
-
+    
     func changeServers(newServers: [String]) -> Void {
         UserDefaults.standard.set(newServers, forKey: "priceServers")
     }
-
+    
     func setCurrentServer(server: String, index: Int) -> Void {
         UserDefaults.standard.set(server, forKey: "currentServer")
         UserDefaults.standard.set(index, forKey: "currentServerIndex")
     }
-
+    
     func getCurrentServerString() -> String {
         return UserDefaults.standard.string(forKey: "currentServer") ?? defaultServerString
     }
-
+    
     func getCurrentServerIndex() -> Int {
         return UserDefaults.standard.integer(forKey: "currentServerIndex")
     }
-
+    
     func addServer(server: String) -> Void {
         var currentServers = self.getServers()
         currentServers.append(server)
         self.changeServers(newServers: currentServers)
     }
-
+    
     func removeServerByString(server: String) -> Void {
         var currentServers = self.getServers()
         if let index = currentServers.firstIndex(of: server) {
@@ -70,33 +84,33 @@ class PriceServer {
             self.changeServers(newServers: currentServers)
         }
     }
-
+    
     func removeServerByIndex(index: Int) -> Void {
         var currentServers = self.getServers()
         currentServers.remove(at: index)
         self.changeServers(newServers: currentServers)
     }
-
+    
     func getCurrentExchange() -> String {
         return UserDefaults.standard.string(forKey: "currentExchange") ?? self.getDefaultExchange()
     }
-
+    
     func changeExchange(newExchange: String) -> Void {
         UserDefaults.standard.set(newExchange, forKey: "currentExchange")
     }
-
+    
     func getExchangeList() -> [String] {
         return localeToExchange[localeConfig.getSavedLocale()] ?? exchangeList
     }
-
+    
     func createSpotBitURL() -> String {
         if self.getCurrentExchange() == "Average" {
             return "http://" + self.getCurrentServerString() + "/now/" + localeConfig.getSavedLocale()
         } else {
-            return "http://" + self.getCurrentServerString() + "/now/" + localeConfig.getSavedLocale() + "/" + self.getCurrentExchange().lowercased()
+            return "http://" + self.getCurrentServerString() + "/now/" + localeConfig.getSavedLocale() + "/" + self.getCurrentExchange().replacingOccurrences(of: " ", with: "").lowercased()
         }
     }
-
+    
     func getSavedExchangeIndex() -> Int {
         if self.getExchangeList().firstIndex(of: self.getCurrentExchange()) == nil {
             self.changeExchange(newExchange: self.getExchangeList()[0])
@@ -104,9 +118,5 @@ class PriceServer {
         } else {
             return self.getExchangeList().firstIndex(of: self.getCurrentExchange()) ?? 0
         }
-    }
-
-    func getSavedExchangeIndex() -> Int {
-        return exchangeList.firstIndex(of: self.getCurrentExchange()) ?? 0
     }
 }
